ad5b9fb77b56077d55e4606f31bce1cc6d9be7cd
FaisalKhan92 <107831838+FaisalKhan92@users.noreply.github.com>
2022-07-13 16:08:00 +0530

    Update model.py

--- ML_Pipeline_Demo/model.py
+++ ML_Pipeline_Demo/model.py
@@ -1,7 +1,7 @@
 # Import modules and packages
 import tensorflow as tf
-import numpy as np
-import matplotlib.pyplot as plt
+# import numpy as np
+# import matplotlib.pyplot as plt
 
 
 # Functions and procedures
@@ -11,36 +11,36 @@ def plot_predictions(train_data, train_labels,  test_data, test_labels,  predict
   """
   plt.figure(figsize=(6, 5))
   # Plot training data in blue
-  plt.scatter(train_data, train_labels, c="b", label="Training data")
+  plt.scatter(train_data, train_labels, c="b", label="Train data")
   # Plot test data in green
-  plt.scatter(test_data, test_labels, c="g", label="Testing data")
+  plt.scatter(test_data, test_labels, c="g", label="Test data")
   # Plot the predictions in red (predictions were made on the test data)
-  plt.scatter(test_data, predictions, c="r", label="Predictions")
+  plt.scatter(test_data, predictions, c="r", label="Prediction")
   # Show the legend
   plt.legend(shadow='True')
   # Set grids
   plt.grid(which='major', c='#cccccc', linestyle='--', alpha=0.5)
   # Some text
   plt.title('Model Results', family='Arial', fontsize=14)
-  plt.xlabel('X axis values', family='Arial', fontsize=11)
-  plt.ylabel('Y axis values', family='Arial', fontsize=11)
+  plt.xlabel('X axis', family='Arial', fontsize=11)
+  plt.ylabel('Y axis', family='Arial', fontsize=11)
   # Show
   plt.savefig('model_results.png', dpi=120)
 
 
 
-def mae(y_test, y_pred):
+def MeanAbsoluteError(b_test, b_pred):
   """
   Calculuates mean absolute error between y_test and y_preds.
   """
-  return tf.metrics.mean_absolute_error(y_test, y_pred)
+  return tf.metrics.mean_absolute_error(b_test, b_pred)
   
 
-def mse(y_test, y_pred):
+def MeanSquareError(b_test, b_pred):
   """
   Calculates mean squared error between y_test and y_preds.
   """
-  return tf.metrics.mean_squared_error(y_test, y_pred)
+  return tf.metrics.mean_squared_error(b_test, b_pred)
 
 
 # Check Tensorflow version
@@ -48,26 +48,18 @@ print(tf.__version__)
 
 
 # Create features
-X = np.arange(-100, 100, 4)
+a = np.arange(1, 60, 4)
 
 # Create labels
-y = np.arange(-90, 110, 4)
+b = np.arange(1, 60, 4)
 
 
 # Split data into train and test sets
-X_train = X[:40] # first 40 examples (80% of data)
-y_train = y[:40]
+a_train = a[:10] # first 40 examples (80% of data)
+b_train = b[:10]
 
-X_test = X[40:] # last 10 examples (20% of data)
-y_test = y[40:]
-
-
-# Take a single example of X
-input_shape = X[0].shape 
-
-# Take a single example of y
-output_shape = y[0].shape
-
+a_test = a[10:] # last 10 examples (20% of data)
+b_test = b[10:]
 
 # Set random seed
 tf.random.set_seed(42)
@@ -81,22 +73,23 @@ model = tf.keras.Sequential([
 # Compile the model
 model.compile(loss = tf.keras.losses.mae,
               optimizer = tf.keras.optimizers.SGD(),
-              metrics = ['mae'])
+              metrics = ['MeanAbsoluteError'])
 
 # Fit the model
-model.fit(X_train, y_train, epochs=100)
+#model.fit(tf.expand_dims(a_train, axis=1), b_train, epochs=100)
+model.fit(a_train, b_train, epochs=100)
 
 
 # Make and plot predictions for model_1
-y_preds = model.predict(X_test)
-plot_predictions(train_data=X_train, train_labels=y_train,  test_data=X_test, test_labels=y_test,  predictions=y_preds)
+b_preds = model.predict(a_test)
+plot_predictions(train_data=a_train, train_labels=b_train,  test_data=a_test, test_labels=b_test,  predictions=b_preds)
 
 
 # Calculate model_1 metrics
-mae_1 = np.round(float(mae(y_test, y_preds.squeeze()).numpy()), 2)
-mse_1 = np.round(float(mse(y_test, y_preds.squeeze()).numpy()), 2)
-print(f'\nMean Absolute Error = {mae_1}, Mean Squared Error = {mse_1}.')
+MeanAbsoluteError_1 = np.round(float(MeanAbsoluteError(b_test, b_preds.squeeze()).numpy()), 2)
+MeanSquareError_1 = np.round(float(MeanSquareError(b_test, b_preds.squeeze()).numpy()), 2)
+print(f'\nMean Absolute Error = {MeanAbsoluteError_1}, Mean Squared Error = {MeanSquareError_1}.')
 
 # Write metrics to file
 with open('metrics.txt', 'w') as outfile:
-    outfile.write(f'\nMean Absolute Error = {mae_1}, Mean Squared Error = {mse_1}.')
+    outfile.write(f'\nMean Absolute Error = {MeanAbsoluteError_1}, Mean Squared Error = {MeanSquareError_1}.')
