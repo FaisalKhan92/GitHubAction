810f1f356b12ef6f4f144922d87a3aec1139f7db
FaisalKhan92 <107831838+FaisalKhan92@users.noreply.github.com>
2022-07-13 16:55:41 +0530

    Update model.py

--- ML_Pipeline_Demo/model.py
+++ ML_Pipeline_Demo/model.py
@@ -71,7 +71,7 @@ model = tf.keras.Sequential([
     ])
 
 # Compile the model
-model.compile(loss = tf.keras.losses.MeanAbsoluteError,
+model.compile(loss =MeanAbsoluteError,
               optimizer = tf.keras.optimizers.SGD(),
               metrics = ['MeanAbsoluteError'])
 
